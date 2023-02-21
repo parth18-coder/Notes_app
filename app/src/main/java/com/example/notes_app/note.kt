5f214cb47e2584c6c8b4d291a9bf848fb96ef932
Parth <banparth57@gmail.com>
2023-02-21 16:21:11 +0530

    Addition of the up button in the app bar of the notes activity

--- app/src/main/java/com/example/notes_app/note.kt
+++ app/src/main/java/com/example/notes_app/note.kt
@@ -7,5 +7,10 @@ class note : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
+
+        setSupportActionBar(findViewById(R.id.notes_toolbar))
+
+        // Get a support ActionBar corresponding to this toolbar and enable the Up button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
